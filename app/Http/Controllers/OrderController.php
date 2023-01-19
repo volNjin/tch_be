5fb39e0e2d79cfc3a6e86828fc83be8d36c03524
Do Minh Hieu <hieu.dm194558@sis.hust.edu.vn>
2023-01-19 18:24:28 +0700

    so tired to write

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Order;
 use App\Models\OrderItem;
+use App\Models\AddressNote;
+use App\Http\Controllers\Admin\ToppingController;
+
 class OrderController extends Controller
 {
     public function addOrder(Request $request){
@@ -18,20 +21,36 @@ class OrderController extends Controller
                 'address' => $request->address,
                 'note' => $request->note,
                 'shipcost' => '15000',
+                'total_price' => $request->total_price,
                 'payment_method' => $request->payment_method,
             ]);
-            // dd($order->id);
+            if(!AddressNote::where('user_id', $request->user_id)
+                            ->where('address', $request->address)
+                            ->exists()){
+                AddressNote::create([
+                    'user_id' => (int)$request->user_id,
+                    'user_name' => $request->user_name,
+                    'address' => $request->address,
+                    'mobile_no' => $request->mobile_no,
+                ]);
+            }
             foreach($request->products as $product){
                 OrderItem::create([
                     'order_id' => $order->id,
                     'product_id' => $product['product_id'],
+                    'product_count' => $product['product_count'],
                     'topping_id' => $product['topping_id'],
+                    'topping_count' => $product['topping_count'],
                     'size' => $product['size'],
                     'price' =>$product['price'],
                 ]);
             }
+            return response([
+                'error' => false
+            ]);
         }catch(\Exception $err){
             return response([
+                'error' => true,
                 'message' => $err->getMessage()
             ]);
         };
@@ -51,6 +70,9 @@ class OrderController extends Controller
                                 ->orderby('id')
                                 ->get();
         
+        foreach($orderItems as $item){
+            $item->topping_id=ToppingController::getTopping($item->topping_id);
+        }                     
         return response([
             'orderItems' => $orderItems
         ]);
