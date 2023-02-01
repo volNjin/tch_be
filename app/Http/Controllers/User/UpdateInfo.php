2ad68ddf22c1f8b6e9826a505dd85f41c933e887
Do Minh Hieu <hieu.dm194558@sis.hust.edu.vn>
2023-02-01 15:07:38 +0700

    fix

--- app/Http/Controllers/User/UpdateInfo.php
+++ app/Http/Controllers/User/UpdateInfo.php
@@ -1,16 +1,14 @@
-<?
+<?php
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\Hash;
 
 class UpdateInfo extends Controller{
     public function update(Request $request){
-        $user=User::where("id",$request->id)->update($request->all());
+        $user=User::where("id",$request->id)->first();
+        $user->update($request->all());
         return response([
             'user'=>$user
         ]);
